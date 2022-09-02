e2c59c604d1bfdb573d7720ffabb122213bb6cbb
Yuran Vita <yuranxd1@gmail.com>
2022-09-01 21:10:21 -0400

    feat: login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,64 @@
-import { SignInContainer } from "./styles";
+import React, { useEffect, useRef, useState } from "react";
 
-const Login = () => {
-  return <h1>kkkkk</h1>;
-};
+import { ContentIntro, FormContent, FormSign, SignInContainer } from "./styles";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
-export default Login;
+import { ReactComponent as Intro } from "../../assets/intro.svg";
+import { parseClassName } from "react-toastify/dist/utils";
+
+function SignIn() {
+  const [typePassword, setTypePassword] = useState("password");
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  function showPassword() {
+    typePassword === "password"
+      ? setTypePassword("text")
+      : setTypePassword("password");
+  }
+
+  return (
+    <SignInContainer>
+      <ContentIntro>
+        <FormContent>
+          <div>
+            <h2>Seja, bem vindo(a)!</h2>
+          </div>
+          <FormSign>
+            <input type="text" name="" placeholder="email" autoFocus />
+            <div>
+              <input
+                type={typePassword}
+                name="password"
+                ref={(passwordRef) => {
+                  if (passwordRef) {
+                    passwordRef.focus();
+                  }
+                }}
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  showPassword();
+                }}
+                autoFocus={false}
+              >
+                {typePassword === "password" ? (
+                  <FiEye width={40} color="#fff" />
+                ) : (
+                  <FiEyeOff width={40} color="#fff" />
+                )}
+              </button>
+            </div>
+            <button type="submit">Login</button>
+          </FormSign>
+        </FormContent>
+        <Intro width="90vh" />
+      </ContentIntro>
+    </SignInContainer>
+  );
+}
+
+export default SignIn;
