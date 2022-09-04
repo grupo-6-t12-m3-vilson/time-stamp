44117a2947e25164f6aec22bf58b5630d829c827
Igor Gomes <igorgomes255@gmail.com>
2022-09-04 13:16:52 -0300

    fix: fixing bugs in themes and userContext

--- src/contexts/UserContext/index.tsx
+++ src/contexts/UserContext/index.tsx
@@ -8,24 +8,23 @@ const UserProvider = ({ children }: IUserProviderProps) => {
 
   const themeDark = () => {
     setTheme(!theme);
-
-    const [card, setCard] = useState([
-      { module: "M3", dia: "05/07/22", sprint: 1 },
-      { module: "M3", dia: "12/07/22", sprint: 2 },
-      { module: "M3", dia: "21/07/22", sprint: 3 },
-      { module: "M3", dia: "28/07/22", sprint: 4 },
-      { module: "M3", dia: "05/08/22", sprint: 5 },
-      { module: "M3", dia: "12/08/22", sprint: 6 },
-      { module: "M3", dia: "19/08/22", sprint: 7 },
-      { module: "M3", dia: "28/08/22", sprint: 8 },
-    ]);
-
-    return (
-      <UserContext.Provider value={{ card, theme, themeDark }}>
-        {children}
-      </UserContext.Provider>
-    );
   };
+  const [card, setCard] = useState([
+    { module: "M3", dia: "05/07/22", sprint: 1 },
+    { module: "M3", dia: "12/07/22", sprint: 2 },
+    { module: "M3", dia: "21/07/22", sprint: 3 },
+    { module: "M3", dia: "28/07/22", sprint: 4 },
+    { module: "M3", dia: "05/08/22", sprint: 5 },
+    { module: "M3", dia: "12/08/22", sprint: 6 },
+    { module: "M3", dia: "19/08/22", sprint: 7 },
+    { module: "M3", dia: "28/08/22", sprint: 8 },
+  ]);
+
+  return (
+    <UserContext.Provider value={{ card, theme, themeDark }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export default UserProvider;
